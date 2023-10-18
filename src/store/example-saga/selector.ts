13ccb6ab5a2a01a8b2e08c3beef62657954ad72a
nhphong0410 <nguyenhongphong281099@gmail.com>
2023-10-18 11:55:45 +0700

    NF-setup-async-thunk: refactor sagas

--- src/store/example-saga/selector.ts
+++
@@ -1,3 +0,0 @@
-import { type AppState } from '@/store';
-
-export const selectNumber = (state: AppState) => state.exampleSaga.number;
---
+++ src/store/sagas/index.ts
@@ -0,0 +1,1 @@
+export * as exampleSaga from './example-saga';
